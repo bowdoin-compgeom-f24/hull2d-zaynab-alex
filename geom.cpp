08000e8cd818d0600a027643cfbe55f23f9a8e2b
Zaynab Azzouzi <zazzouzi@bowdoin.edu>
2024-09-23 21:40:22 -0400

    good start

--- geom.cpp
+++ geom.cpp
@@ -12,8 +12,12 @@ using namespace std;
    is to the left of ab, and negative if c is to the right of ab
  */
 int signed_area2D(point2d a, point2d b, point2d c) {
-
-  return 1; 
+  int Ax = b.x - a.x; 
+  int Ay = b.y - a.y; 
+  int Bx = c.x -a.x; 
+  int By = c.y - aa.y; 
+  return Ax By - Ay Bx; 
+ 
 }
 
 
@@ -21,8 +25,11 @@ int signed_area2D(point2d a, point2d b, point2d c) {
 /* **************************************** */
 /* return 1 if p,q,r collinear, and 0 otherwise */
 int collinear(point2d p, point2d q, point2d r) {
-  
-  return 1; 
+  int area = signed_area2D(p, q, r);
+  if (area == 0){
+    return 1;
+  }
+  return 0;  
 }
 
 
@@ -30,27 +37,30 @@ int collinear(point2d p, point2d q, point2d r) {
 /* **************************************** */
 /* return 1 if c is  strictly left of ab; 0 otherwise */
 int left_strictly(point2d a, point2d b, point2d c) {
-  
-  return 1; 
+  int area = signed_area2D(a, b, c);
+  if (area > 0){
+    return 1;
+  }
+  return 0; 
 }
 
 
 /* return 1 if c is left of ab or on ab; 0 otherwise */
 int left_on(point2d a, point2d b, point2d c) {
-
-  return 1; 
+  int area = signed_area2D(a, b, c);
+  if (area >= 0){
+    return 1;
+  }
+  return 0; 
 }
 
-
-
-// compute the convex hull of pts, and store the points on the hull in hull
-void graham_scan(vector<point2d>& pts, vector<point2d>& hull ) {
-
-  printf("hull2d (graham scan): start\n"); 
+vector<point2d>& initializer(vector<point2d>& pts, vector<point2d>& hull){
+  printf("hull2d (graham scan): start\n");
   hull.clear(); //should be empty, but clear it to be safe
 
-  //just for fun: at the moment we set the hull as the bounding box of
-  //pts.  erase this and insert your code instead
+
+ //just for fun: at the moment we set the hull as the bounding box of
+ //pts.  erase this and insert your code instead
   int x1, x2, y1, y2;
   if (pts.size() > 0) {
     x1 = x2 = pts[0].x;
@@ -62,14 +72,76 @@ void graham_scan(vector<point2d>& pts, vector<point2d>& hull ) {
       if (pts[i].y < y1) y1 = pts[i].y;
       if (pts[i].y > y2) y2 = pts[i].y;
     }
-    point2d p1 = {x1,y1}, p2 = {x2, y1}, p3 = {x2, y2}, p4 = {x1, y2}; 
+    point2d p1 = {x1,y1}, p2 = {x2, y1}, p3 = {x2, y2}, p4 = {x1, y2};
     hull.push_back(p1);
     hull.push_back(p2);
     hull.push_back(p3);
     hull.push_back(p4);
   }
+  printf("hull2d (graham scan): end\n");
+  return hull;
+}
+
+vector<point2d>& sortPoints(vector<point2d>& pts){
+  //do sorting and use leftOf as basis
   
+  return pts;
+}
+
+// compute the convex hull of pts, and store the points on the hull in hull
+void graham_scan(vector<point2d>& pts, vector<point2d>& hull ) {
+  //Algorithm GrahamScan (input: points P )
+    //Find interior point p0 (instead of an interior point, can pick the lowest point)
+    //Sort all other points ccw around p0; denote them p1, p2, ...pn−1 in this order.
+    //Initialize stack S = (p2, p1)
+    //for i = 3 to n-1 do
+–     //if pi is left of (second(S), first(S)): 
+        //push pi on S
+–     //else:
+∗       //repeat: pop S while pi is right of (second(S), first(S))
+∗       //push pi on S
+
+  printf("hull2d (graham scan): start\n"); 
+  hull.clear(); //should be empty, but clear it to be safe
+
+  point2d p0;
+  int minY = 10000;
+  for( int i = 0; i < pts.size(); i ++){
+    if ( pts[i].y < minY){
+      minY = pts[i].y;
+      p0 = pts[i];
+    }
+    else if(pts[i].y == minY && pts[i].x > p0.x){
+      minY = pts[i].y;
+      p0 = pts[i];
+    }
+
+  }
+  //now we have p0 guaranteed to be on hull
+  //now we sort points
+  vector<point2d>& sortedPoints = sortPoints(pts);
+  //assuming hull is acting as our stack
+  hull.push_back(p0);
+  hull.push_back(sortedPoints[1]);//does sortedPoints include p0, the index in this line depends on that
+  int top1 = sortedPoints.size() - 1
+  int top2 = sortedPoints.size() - 2
+  for (int i = 3; i < sortedPoints.size(); i++){//is it really i = 3?
+    if(left_on(sortedPoints[top2], sortedPoints[top1], sortedPoints[i])){
+      hull.push_back(sortedPoints[i]);
+    }
+    else{
+      hull.pop_back();
+      //while points[i] is on right of edge a,b
+      while(!(left_on(sortedPoints[top2], sortedPoints[top1], sortedPoints[i]))){
+        hull.pop_back();
+      }
+      hull.push_back(sortedPoints[i]);
+    }
+  }
+  //return hull
   printf("hull2d (graham scan): end\n"); 
   return; 
 }
+
+
 
