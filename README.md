dae77ee8c79a628e5427d1c3ba7119dcf0955cf0
Zaynab Azzouzi <zazzouzi@bowdoin.edu>
2024-10-02 23:13:07 -0400

    everything is working

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ compute the convex hull of a set of given points. The initialization of the poin
 viewhull.cpp file, and the computation of the set of points contained in the convex hull was done in
 the geom.cpp file. 
 
-Briefly, the way we decided to implement the graham scan was to first find the lowest point amoung 
+Briefly, the way we decided to implement the graham scan was to first find the lowest point among 
 all of the points (rightmost point if there was a tie), and we called this point p0. We then sorted
 the points by their counter-clockwise angle with respect to p0 by implementing a comparator and 
 using a built-in method (qsort()) to sort based on the comparator. Some caveats here included 
@@ -16,21 +16,22 @@ the issue of points that created the same angle with p0 (i.e a set of points tha
 with each other and also p0). To handle this case, we simply ordered the points from closest to 
 farthest from p0 in the case of a tie (using the magnitude of the Manhattan distance as a metric). 
 
-Once all of our points were sorted, we then traversed the points in counter-clockwise order 
+Once all of our points were sorted, we then traversed them in order 
 to build up a chain of points that are on the convex hull. This was done by comparing a potential 
-point to be added to the convex hull to the LINE connects the most recently added two points in our
-current chain of points in the convex hull: if the potential point is to the left of the line, 
+point to be added to the convex hull to the line that connects the most recently added two points in our
+current chain of points in the convex hull: if the potential point is strictly to the left of the line, 
 then this point can be added to the convex hull, and we push it to the chain. However, if the 
 potential point is to the right of the line segment, we need to pop off the last point in the 
-current chain of points in the convex hull and then try our next point in sorted order and see 
-if it can be added to the convex hull. Note that a vertex could theoretically create a series
-of repeated pops off of our current chain of points in the convex hull.
+current chain of points in the convex hull and check if our potential point is strictly left of the line segment
+between the new 2 most recently points in our current chain of points in the hull. If yes then it is popped 
+onto the hull. Note that a vertex could theoretically create a series of repeated pops off of our 
+current chain of points in the convex hull.
 
 One issue that arises is that of colinearity: however, due to the way that the points were sorted, 
 the algorithm described above correctly finds the convex hull even with collinear points.
 
 Lastly, we created and copied and pasted several initializers (functions that create, draw and 
-display points on the screen) in viewhull.cpp. Our main functoin takes in an input n from the user 
+display points on the screen) in viewhull.cpp. Our main function takes in an input n from the user 
 that tells the initializer how many points to make, runs the initialier, runs graham scan, and then 
 draws the hull created by graham scan. 
 
@@ -39,6 +40,6 @@ number of points one intends to generate. In the main method, specify which poin
 configuration you would like to use first. From here, recompute the set of points and find the 
 convex hull using Graham Scan for all the other point initialization configurations, simply 
 press the key "i" repeatedly. This will re-initialize and compute the convex hull for the point 
-configurations: [TODO] in order. Note: for a given set of initialized poitns, the points in the
+configurations: [TODO] in order. Note: for a given set of initialized points, the points in the
 convex hull will be printed out in the output console. 
  
--- geom.cpp
+++ geom.cpp
@@ -10,7 +10,6 @@ using namespace std;
 
 // GLOBAL VARIABLES
 point2d p0; // location of the bottommost point, guaranteed to be an extreme point
-const double EPSILON = 1 * pow(10,-6);
 
 /* **************************************** */
 /* 
@@ -133,48 +132,31 @@ int compare_angles(const void* ppoint1, const void* ppoint2) {
   point2d point1 = *ppoint1_tmp;
   point2d point2 = *ppoint2_tmp;
 
-  //printf("point1 x being compared: %d\n", point1.x);
-  //printf("point1 y being compared: %d\n", point1.y);
-  //printf("point2 x being compared: %d\n", point2.x);
-  //printf("point2 y being compared: %d\n", point2.y);
+  //compute counter clockwise angles of points 1 and 2 with p0 as a reference
   double angle1 = atan2((double) (point1.y - p0.y), (double) (point1.x - p0.x));
   double angle2 = atan2((double) (point2.y - p0.y), (double) (point2.x - p0.x));
 
   if (angle1 < angle2) {
-    //printf("angle 1: %f\n", angle1);
-    //printf("angle 2: %f\n", angle2);
-    //printf("angle 1 < angle 2\n");
     // we want angle1 (point 1) sorted before angle2 (point 2)
     return -1;
   } 
   if (angle1 > angle2) {
-    //printf("angle 1: %f\n", angle1);
-    //printf("angle 2: %f\n", angle2);
-    //printf("angle 1 > angle 2\n");
     // we want angle2 (point 2) sorted before angle1 (point 1)
     return 1;
   } 
-  //dealing with colinear points
+
+  //measuring manhattan distance of points from p0
   int manhattan_distance_point1 = abs((point1.x - p0.x) + (point1.y - p0.y));
   int manhattan_distance_point2 = abs((point2.x - p0.x) + (point2.y - p0.y));
-
-  //if points 1 and 2 make the same angle and are both right of the x value of p0 or have the same x as p0
+  //if points 1 and 2 make the same angle/are collinear
   if (angle1 == angle2){
-    //check which point is closer
+    //check if point 1 is closer or if they are the same point
     if((manhattan_distance_point1 < manhattan_distance_point2) || (manhattan_distance_point1 == manhattan_distance_point2)){
-      //if point 1 is closer to p0 then sort first
-      //printf("on right side, angles are the same, but p1 closer to p0\n");
-      //OR // points are the same. Mark one of them for deletion (TODO)
+      //if so then point 1 should be sorted first
       return -1;
     }
-
-    if (manhattan_distance_point1 == manhattan_distance_point2) {
-      // points are the same. Mark one of them for deletion (TODO)
-      printf("implement this later.\n");
-      return -1; // change this later
-    }
-
-    if (manhattan_distance_point1 > manhattan_distance_point2) {
+    //else then point 2 is closer and should be sorted first
+    else{
       return 1;
     } 
   }
@@ -200,12 +182,6 @@ void graham_scan(vector<point2d>& pts, vector<point2d>& hull ) {
   printf("hull2d (graham scan): start\n"); 
   hull.clear(); //should be empty, but clear it to be safe
 
-  // print all of the points
-  for (int i = 0; i<pts.size(); i++) {
-    printf("x of point: %d\n", pts[i].x);
-    printf("y of point: %d\n", pts[i].y);
-  }
-
   // set point p0 globally
   double minY = DBL_MAX;
   int p0_index;
@@ -224,84 +200,44 @@ void graham_scan(vector<point2d>& pts, vector<point2d>& hull ) {
       p0_index = i;
     }
   }
+  //now we have p0 guaranteed to be on the hull
 
   // delete p0 from our pts, since it is guaranteed to be an extreme point and doesn't need sorting
-  pts.erase(pts.begin() + p0_index); // TODO test that this is working correctly
+  pts.erase(pts.begin() + p0_index); 
 
-  //now we have p0 guaranteed to be on the hull, and the remaining points need to be added to hull
-  //now we sort points
-  printf("RIGHT BEFORE SORTING ORDER:\n");
   // convert vector to an array
   point2d* pts_array = &pts[0];
-  for (int i = 0; i<pts.size(); i++) {
-    printf("x of point: %d\n", pts[i].x);
-    printf("y of point: %d\n", pts[i].y);
-  }
-  //sort(pts.begin(), pts.end(), compare_angles);  
+  //now we sort points
   qsort((void*) &pts_array[0], pts.size(), sizeof(point2d), compare_angles);
-  //print points in sorted order
-  printf("SORTED ORDER:\n");
-  printf("PO x: %d\n", p0.x);
-  printf("PO y: %d\n", p0.y);
-  for (int i = 0; i<pts.size(); i++) {
-    printf("x of point: %d\n", pts[i].x);
-    printf("y of point: %d\n", pts[i].y);
-  }
-
-  //assuming hull is acting as our stack
+  
+  //push p0 and the first point in sorted pts onto hull
   hull.push_back(p0);
   hull.push_back(pts[0]);
-  int i = 1;//does sortedPoints include p0, the index in this line depends on that
+  int i = 1;
   // we dont increment every iteration, because we only want to go to the next element when we add 
   // a point to the hull, not when we pop one off and backtrack
   while (i < pts.size()){
-    printf("loop entered\n");
+    //indices of 2 most recently added points in hull
     int top1 = hull.size() - 1;
     int top2 = hull.size() - 2;
 
-    // experimental debugging
+    // if hull size is ever 1 then add the point we are currently looking at
     if (hull.size() == 1) {
-      printf("hull currently only has p0\n");
       hull.push_back(pts[i]);
-      printf("see the hull has 2 elements now\n");
-      printf("%lu\n", hull.size());
       i++;
       continue; 
     }
-    /* printf("hull[top2] x: %d\n", hull[top2].x);
-    printf("hull[top2] y: %d\n", hull[top2].y);
-    printf("hull[top1] x: %d\n", hull[top1].x);
-    printf("hull[top1] y: %d\n", hull[top1].y);
-    printf("pts[i] x: %d\n", pts[i].x);
-    printf("pts[i] y: %d\n", pts[i].y); */
-    if(left_strictly(hull[top2], hull[top1], pts[i])){//i changed it to strictly_left to deal with colinearity
-      printf("pushing pts[i]\n");
+    //check if current point is strictly left of the line between the 2 most recently added points in hull 
+    if(left_strictly(hull[top2], hull[top1], pts[i])){
       hull.push_back(pts[i]);
       i++; // only incremement after push 
     }
+    //if not then pop most recently added point off the hull
     else{
-      printf("popping something\n");
-      point2d lastElement = hull.back();
-      printf("element being popped x: %d\n", lastElement.x);
-      printf("element being popped x: %d\n", lastElement.y);
       hull.pop_back();
-      //while points[i] is on right of edge a,b OR points[i] is colinear with edge a,b
-      //while(!(left_on(pts[top2], pts[top1], pts[i]))){
-       // hull.pop_back();
-      //}
-      //hull.push_back(pts[i]);
     }
   }
 
-  // if there are 3 collinear points at the end, we want to delete the middle one from the hull
-  // TODO - do we need this? 
-  /* int hull_size = hull.size();
-  if (collinear(p0, hull[hull_size-1], hull[hull_size-2]) == 1) {
-    printf("conditional entered.\n");
-    hull.pop_back();
-  } */
-
-  //return hull
   printf("hull2d (graham scan): end\n"); 
   return; 
 }
